76517f58bc32c43c409b94f612d232f51450a72e
Irwin Razaghi <irazaghi@atlassian.com>
2021-03-18 17:23:20 +1100

    Set up initial application structure.

--- src/pages/search/main.tsx
+++ src/pages/search/main.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { ReactElement } from "react";
+import "./styles.sass";
 
-function App() {
+export default function SearchPage(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -22,5 +20,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
--- src/pages/search/test.tsx
+++ src/pages/search/test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+
+import SearchPage from "./main";
 
 test("renders learn react link", () => {
-  render(<App />);
+  render(<SearchPage />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
